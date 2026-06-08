9c74eef2b0aa5441089d952d61e34e1b70aaeab8
MedinImmersion <medinimmersion@gmail.com>
2026-06-08 02:05:34 +0100

    Create groups.js with group management functions

---
+++ db/db/groups.js
@@ -0,0 +1,12 @@
+'use strict';
+// Stub module
+module.exports = {
+  getGroups: async () => [],
+  getGroup: async () => null,
+  createGroup: async () => ({}),
+  updateGroup: async () => ({}),
+  deleteGroup: async () => ({}),
+  getGroupMembers: async () => [],
+  addGroupMember: async () => ({}),
+  removeGroupMember: async () => ({}),
+};
