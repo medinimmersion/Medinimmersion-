95ad2ce9e0dca3ece18c13ebfc0824b665d4f268
MedinImmersion <medinimmersion@gmail.com>
2026-06-08 02:04:30 +0100

    Create visits.js with stub module functions

---
+++ db/visits.js
@@ -0,0 +1,9 @@
+'use strict';
+// Stub module
+module.exports = {
+  getVisitStats: async () => ({ visits: [], total: 0 }),
+  getWhatsAppConfig: async () => ({}),
+  saveWhatsAppConfig: async () => ({}),
+  getBanner: async () => ({}),
+  saveBanner: async () => ({}),
+};
